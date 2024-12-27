0d67703ca158ab1787d5009534ff5c9a151b1a3b
Tim Jones <tim.jones@forensic-testing.co.uk>
2024-12-27 15:17:01 +0000

    Flushing stdout to collect debug messages

--- app/main.py
+++ app/main.py
@@ -30,19 +30,23 @@ class Shell:
             getattr(self, self.command)()
         else:
             sys.stdout.write(f"{self.command}: command not found\n")
+            sys.stdout.flush()
         self.handle()
 
     def exit(self):
         if not self.arguments:
             sys.stdout.write("calling sys.exit(0)\n")
+            sys.stdout.flush()
             sys.exit(0)
         if len(self.arguments) == 1:
             argument = self.arguments[0]
             sys.stdout.write(f"calling sys.exit({argument})\n")
+            sys.stdout.flush()
             sys.exit(argument)
 
         sys.stdout.write(f"{self.command}: invalid number of arguments\n")
         sys.stdout.write("calling sys.exit(0)\n")
+        sys.stdout.flush()
         sys.exit(0)
 
 
