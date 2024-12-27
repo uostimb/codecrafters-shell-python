64ce00ca285ef86a9487ff69741eb57488f6da5e
Tim Jones <tim.jones@forensic-testing.co.uk>
2024-12-27 16:17:05 +0000

    Task4: Added documentation and exit with code 1 on unhandled error

--- app/main.py
+++ app/main.py
@@ -9,40 +9,92 @@ class Shell:
         self.handle()
 
     def handle(self):
-        self.reset()
-        self.read_commands()
-        self.handle_commands()
+        """
+        Main program loop.
 
-    def reset(self):
+        * Resets internal memory to initial state (to prevent reuse of
+        prior arguments)
+        * read users commands and arguments
+        * cast numeric integer-string arguments to ints
+        * handle users commands
+        * recursively calls self
+
+        Will recursively loop until the program is exited (e.g. by
+        calling self.exit())
+        """
+        self._reset()
+        self._read_commands()
+        self._handle_commands()
+        self.handle()  # recursively loop until explicitly exited
+
+    def _reset(self):
+        """
+        Reset self.command and self.arguments to initial state.
+        """
         self.command = ''
         self.arguments = []
 
-    def read_commands(self):
+    def _read_commands(self):
+        """
+        Read users commands and arguments.
+
+        * Write "$ " to std out
+        * Read users input
+        * Split the input on " " to [[command], [arguments, ...]]"
+        * Cast any integer string arguments to ints
+        """
         sys.stdout.write("$ ")
         commands = input()
         inputs = commands.split(" ")
         self.command = inputs[0]
-        if len(inputs) > 1:
-            self.arguments = inputs[1:]
+        arguments = inputs[1:]
+        # Cast str integer arguments to ints
+        for arg in arguments:
+            if isinstance(arg, str) and arg.isdigit():
+                self.arguments.append(int(arg))
+            else:
+                self.arguments.append(arg)
 
-    def handle_commands(self):
+    def _handle_commands(self):
+        """
+        Handle command in self.command
+
+        If the given command name matches an attribute (method name) on
+        self then call that method.
+
+        If the given command name doesn't match any attributes (method
+        names) on self then write an error message to stdout.
+        """
         if hasattr(self, self.command):
             getattr(self, self.command)()
         else:
             sys.stdout.write(f"{self.command}: command not found\n")
             sys.stdout.flush()
-        self.handle()
 
     def exit(self):
+        """
+        Handle exit commands.
+
+        If no arguments were passed to the exit command then exit the
+        application with exit code 0.
+
+        If a single argument was passed to the exit command then exit
+        the application using the argument as the exit code.
+
+        If multiple arguments were passed to the exit command, write an
+        error message to stdout and then exit with exit code 0.
+
+        Default to exiting program with exit code 1.
+        """
         if not self.arguments:
-            sys.exit(0)
+            sys.exit()
         if len(self.arguments) == 1:
             argument = self.arguments[0]
             sys.exit(argument)
 
         sys.stdout.write(f"{self.command}: invalid number of arguments\n")
         sys.stdout.flush()
-        sys.exit(0)
+        sys.exit(1)
 
 
 def main():
