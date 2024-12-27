ddc024b84cb6d8b47849f6e1a8c65cf34f0fe3d1
Tim Jones <tim.jones@forensic-testing.co.uk>
2024-12-27 14:38:11 +0000

    Test4: Refactor to use a Shell class and add abilty to handle an exit command

--- app/main.py
+++ app/main.py
@@ -1,15 +1,43 @@
 import sys
 
 
-def main():
-    while True:
+class Shell:
+    def __init__(self):
+        self.command = ''
+        self.arguments = []
+        self.handle()
+
+    def handle(self):
+        self.read_command()
+        self.handle_commands()
+
+    def read_command(self):
         sys.stdout.write("$ ")
+        commands = input()
+        inputs = commands.split(" ")
+        self.command = inputs[0]
+        if len(inputs) > 1:
+            self.arguments = inputs[1:]
 
-        # Wait for user input
-        command = input()
+    def handle_commands(self):
+        if hasattr(self, self.command):
+            getattr(self, self.command)()
+        else:
+            sys.stdout.write(f"{self.command}: command not found\n")
+        self.handle()
 
-        # If `command` isn't a valid value, return an error message
-        sys.stdout.write(f"{command}: command not found\n")
+    def exit(self):
+        if not self.arguments:
+            exit(0)
+        if len(self.arguments) == 1:
+            exit(self.arguments[0])
+
+        sys.stdout.write(f"{self.command}: invalid number of arguments\n")
+        exit(0)
+
+
+def main():
+    Shell()
 
 
 if __name__ == "__main__":
